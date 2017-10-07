92d1c65215f076e9a7c2e6e48356a1b146a1f731
NhanNT <nhannt.bk@gmail.com>
2017-10-07 11:13:53 +0700

    Create data service,utility service and notification service

--- src/app/core/domain/loggedin.user.ts
+++ src/app/core/domain/loggedin.user.ts
@@ -1,70 +1,70 @@
 export class LoggedInUser {
-  private _id: string;
-  private _access_token: string;
-  private _username: string;
-  private _fullName: string;
-  private _email: string;
-  private _avatar: string;
+    private _id: string;
+    private _access_token: string;
+    private _username: string;
+    private _fullName: string;
+    private _email: string;
+    private _avatar: string;
 
-  constructor(
-    access_token: string,
-    username: string,
-    fullName: string,
-    email: string,
-    avatar: string
-  ) {
-    this._access_token = access_token;
-    this._username = username;
-    this._fullName = fullName;
-    this._email = email;
-    this._avatar = avatar;
-  }
+    constructor(
+        access_token: string,
+        username: string,
+        fullName: string,
+        email: string,
+        avatar: string
+    ) {
+        this._access_token = access_token;
+        this._username = username;
+        this._fullName = fullName;
+        this._email = email;
+        this._avatar = avatar;
+    }
 
-  public get id(): string {
-    return this._id;
-  }
+    public get id(): string {
+        return this._id;
+    }
 
-  public set id(value: string) {
-    this._id = value;
-  }
+    public set id(value: string) {
+        this._id = value;
+    }
 
-  public get access_token(): string {
-    return this._access_token;
-  }
+    public get access_token(): string {
+        return this._access_token;
+    }
 
-  public set access_token(value: string) {
-    this._access_token = value;
-  }
+    public set access_token(value: string) {
+        this._access_token = value;
+    }
 
-  public get username(): string {
-    return this._username;
-  }
+    public get username(): string {
+        return this._username;
+    }
 
-  public set username(value: string) {
-    this._username = value;
-  }
+    public set username(value: string) {
+        this._username = value;
+    }
 
-  public get fullName(): string {
-    return this._fullName;
-  }
+    public get fullName(): string {
+        return this._fullName;
+    }
 
-  public set fullName(value: string) {
-    this._fullName = value;
-  }
+    public set fullName(value: string) {
+        this._fullName = value;
+    }
 
-  public get email(): string {
-    return this._email;
-  }
+    public get email(): string {
+        return this._email;
+    }
 
-  public set email(value: string) {
-    this._email = value;
-  }
+    public set email(value: string) {
+        this._email = value;
+    }
 
-  public get avatar(): string {
-    return this._avatar;
-  }
+    public get avatar(): string {
+        return this._avatar;
+    }
 
-  public set avatar(value: string) {
-    this._avatar = value;
-  }
+    public set avatar(value: string) {
+        this._avatar = value;
+    }
 }
--- src/app/core/services/authen.service.ts
+++ src/app/core/services/authen.service.ts
@@ -6,52 +6,52 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AuthenService {
-  constructor(private _http: Http) {}
+    constructor(private _http: Http) {}
 
-  login(username: string, password: string) {
-    const body = `userName=${encodeURIComponent(username)}
+    login(username: string, password: string) {
+        const body = `userName=${encodeURIComponent(username)}
     &password=${encodeURIComponent(password)}
     &grant_type=password`;
-    const headers = new Headers();
-    headers.append('Content-Type', 'application/x-www-form-urlencoded');
-    const options = new RequestOptions({ headers: headers });
-    return this._http
-      .post(`${SystemConstants.BASE_API}/api/oauth/token`, body, options)
-      .map((respone: Response) => {
-        const user: LoggedInUser = respone.json();
-        if (user && user.access_token) {
-          localStorage.removeItem(SystemConstants.CURRENT_USER);
-          localStorage.setItem(
-            SystemConstants.CURRENT_USER,
-            JSON.stringify(user)
-          );
-        }
-      });
-  }
-
-  logout() {}
-
-  isUserAuthenticated(): boolean {
-    const user = localStorage.getItem(SystemConstants.CURRENT_USER);
-    return user ? true : false;
-  }
-
-  getLoggedInUser(): any {
-    let user: LoggedInUser;
-    if (this.isUserAuthenticated()) {
-      const userData = JSON.parse(
-        localStorage.getItem(SystemConstants.CURRENT_USER)
-      );
-      user = new LoggedInUser(
-        userData.access_token,
-        userData.username,
-        userData.fullName,
-        userData.email,
-        userData.avatar
-      );
-    } else {
-      user = null;
+        const headers = new Headers();
+        headers.append('Content-Type', 'application/x-www-form-urlencoded');
+        const options = new RequestOptions({ headers: headers });
+        return this._http
+            .post(`${SystemConstants.BASE_API}/api/oauth/token`, body, options)
+            .map((respone: Response) => {
+                const user: LoggedInUser = respone.json();
+                if (user && user.access_token) {
+                    localStorage.removeItem(SystemConstants.CURRENT_USER);
+                    localStorage.setItem(
+                        SystemConstants.CURRENT_USER,
+                        JSON.stringify(user)
+                    );
+                }
+            });
     }
-    return user;
-  }
+
+    logout() {}
+
+    isUserAuthenticated(): boolean {
+        const user = localStorage.getItem(SystemConstants.CURRENT_USER);
+        return user ? true : false;
+    }
+
+    getLoggedInUser(): any {
+        let user: LoggedInUser;
+        if (this.isUserAuthenticated()) {
+            const userData = JSON.parse(
+                localStorage.getItem(SystemConstants.CURRENT_USER)
+            );
+            user = new LoggedInUser(
+                userData.access_token,
+                userData.username,
+                userData.fullName,
+                userData.email,
+                userData.avatar
+            );
+        } else {
+            user = null;
+        }
+        return user;
+    }
 }
