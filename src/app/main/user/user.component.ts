6318695ffddb92f73d1a2a53b564f014dd8be8f6
NhanNT <nhannt.bk@gmail.com>
2017-10-14 23:05:37 +0700

    Show user list

--- src/app/main/user/user.component.ts
+++ src/app/main/user/user.component.ts
@@ -1,15 +1,96 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {ModalDirective} from 'ngx-bootstrap';
+import {DataService} from '../../core/services/data.service';
+import {NotificationService} from '../../core/services/notification.service';
+import {MessageConstants} from '../../core/common/message.constants';
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+    selector: 'app-user',
+    templateUrl: './user.component.html',
+    styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+    @ViewChild('modalAddEdit')
+    public modalAddEdit: ModalDirective;
+    public pageIndex = 1;
+    public pageSize = 10;
+    public pageDisplay = 10;
+    public filter = '';
+    public totalRow: number;
+    public entity: any;
 
-  ngOnInit() {
-  }
+    users: any[];
 
+    constructor(private dataService: DataService, private notifyService: NotificationService) {
+    }
+
+    ngOnInit() {
+        this.loadData();
+    }
+
+    saveChange(isValid: boolean) {
+        console.log(isValid);
+        if (isValid) {
+            if (this.entity.Id === undefined) {
+                this.dataService.post('/api/appUser/add', JSON.stringify(this.entity)).subscribe((response: any) => {
+                    this.loadData();
+                    this.modalAddEdit.hide();
+                    this.notifyService.printSuccessMessage(MessageConstants.CREATED_OK_MSG);
+                }, error => this.dataService.handleError(error));
+            } else {
+                this.dataService.put('/api/appUser/update', JSON.stringify(this.entity)).subscribe((response: any) => {
+                    this.loadData();
+                    this.modalAddEdit.hide();
+                    this.notifyService.printSuccessMessage(MessageConstants.UPDATED_OK_MSG);
+                }, error => this.dataService.handleError(error));
+            }
+        }
+    }
+
+    loadData() {
+        this.dataService.get(
+            `/api/appUser/getlistpaging?page=${this.pageIndex}&pageSize=${this.pageSize}&filter=${this.filter}`
+        ).subscribe((response: any) => {
+            this.users = response.Items;
+            this.pageIndex = response.PageIndex;
+            this.pageSize = response.PageSize;
+            this.totalRow = response.TotalRows;
+        });
+    }
+
+    pageChanged(event: any): void {
+        this.pageIndex = event.page;
+        this.loadData();
+    }
+
+    showAddModal() {
+        this.entity = {};
+        this.modalAddEdit.show();
+    }
+
+    showEditModal(id: any) {
+        this.entity = {};
+        this.loadRole(id);
+        this.modalAddEdit.show();
+    }
+
+    loadRole(id: any) {
+        this.dataService.get(`/api/appUser/detail/${id}`).subscribe((response) => {
+            this.entity = response;
+        });
+    }
+
+    deleteRole(id: any) {
+        this.notifyService.printConfirmationDialog(MessageConstants.CONFIRM_DELETE_MSG, () => this.deleteRoleConfirm(id));
+    }
+
+    deleteRoleConfirm(id: any) {
+        this.dataService.delete(`/api/appUser/delete`, 'id', id).subscribe((response) => {
+            this.notifyService.printSuccessMessage(MessageConstants.DELETED_OK_MSG);
+            this.loadData();
+        }, error => {
+            this.notifyService.printErrorMessage(MessageConstants.SYSTEM_ERROR_MSG);
+        });
+    }
 }
--- src/app/main/user/user.module.ts
+++ src/app/main/user/user.module.ts
@@ -2,6 +2,10 @@ import {Routes, RouterModule} from '@angular/router';
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {UserComponent} from './user.component';
+import {FormsModule} from '@angular/forms';
+import {ModalModule, PaginationModule} from 'ngx-bootstrap';
+import {DataService} from '../../core/services/data.service';
+import {NotificationService} from '../../core/services/notification.service';
 
 const userRoutes: Routes = [
     {path: '', redirectTo: 'index', pathMatch: 'full'},
@@ -11,8 +15,12 @@ const userRoutes: Routes = [
 @NgModule({
     imports: [
         CommonModule,
-        RouterModule.forChild(userRoutes)
+        FormsModule,
+        RouterModule.forChild(userRoutes),
+        PaginationModule.forRoot(),
+        ModalModule.forRoot()
     ],
+    providers: [DataService, NotificationService],
     declarations: [UserComponent]
 })
 export class UserModule {
